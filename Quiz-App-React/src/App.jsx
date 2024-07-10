745b33eda2df5297a109f078821b28f755615158
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-07-10 12:40:12 +0500

    Quiz-App made using React + vite

---
+++ Quiz-App-React/src/App.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+  const [score, setScore] = useState(0)
+
+  const [showScore, setShowScore] = useState(false)
+
+  const questions = [
+    {
+      question:"What is the capital of Canada?",
+      options:["Toronto","Ottawa","Montreal"],
+      answer:'Ottawa'
+    },
+    {
+      question:"What is the longest river in the world?",
+      options:["The Nile River","The Indus River","The Yangtze River"],
+      answer:"The Nile River"
+    },
+    {
+      question:"Which desert is the largest in the world?",
+      options:["The Sahara Desert","The Gobi Desert","The Kalahari Desert"],
+      answer:"The Sahara Desert"
+    },
+    {
+      question:"What country has the most natural lakes?",
+      options:["America","Russia","Canada"],
+      answer:"Canada"
+    },
+    {
+      question:"What is the capital city of Australia?",
+      options:["Canberra","Perth","Malbourne"],
+      answer:"Canberra"
+    },
+  ]
+
+
+  function handleOptionClick(selectedOption){
+
+    // score
+    if (selectedOption === questions[currentQuestion].answer) {
+      setScore(score + 1)
+    }
+
+    // next Question
+    const nextQusetion = currentQuestion + 1
+    if (nextQusetion < questions.length) {
+      setCurrentQuestion(nextQusetion)
+    }
+
+    // if currentQuestion is last Question
+    else{
+      setShowScore(true)
+    }
+
+  }
+
+  return (
+    <>
+    {showScore ? 
+    (<div className='score'>
+      <h3>You got {score} questions right out of {questions.length}</h3>
+    </div>)
+
+    :(<div className='main-container'>
+
+      <div className='box'>
+       <h1>Quiz App</h1>
+
+    <h2>{questions[currentQuestion].question}</h2>
+
+    {/* option container */}
+
+    <div className='option-container'>
+      
+    <h2>{questions[currentQuestion].options.map((option,index)=> {
+
+      return <button key={index} onClick={()=>handleOptionClick(option)}>{option}</button>
+
+    })}</h2>
+
+</div>
+
+    </div>
+
+    </div>)}
+
+    </>
+  )
+}
+
+export default App
